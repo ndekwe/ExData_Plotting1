c82404b827cb74a2813a99ba6e41dbb71b89c75b
Alexis <ndekwe@gmail.com>
2014-10-13 01:00:57 +0200

    This script draws all 4 plots automatically into different files

--- plotsMainScripts.R
+++ plotsMainScripts.R
@@ -1,7 +1,6 @@
 print("TO BE DONE MANUALLY")
 print("Before running the scripts make sure you have <<sqldf>> package installed ")
 print( "This package will be used while loading the dataset subset <<read.csv.sql>>")
-print(" You can install it by running <<install.packages("sqldf")>>")
 
 #install.packages("sqldf")
 
@@ -9,9 +8,9 @@ print(" You can install it by running <<install.packages("sqldf")>>")
 data <- read.csv.sql("household_power_consumption.txt", sql = "SELECT * from file WHERE Date in ('1/2/2007', '2/2/2007')",sep = ";", header = TRUE)
 
 #closing "sql" connection
-close()
+#close()
 
-library(plyr)
+#library(plyr)
 
 Date_and_Time <- paste(data$Date, data$Time)
 Date_and_Time <-as.Date(Date_and_Time)
